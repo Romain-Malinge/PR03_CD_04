555e9618b500c098d1f1181d71be07930b962eb2
Julien <julien2922@outlook.fr>
2022-12-07 11:22:14 +0100

    J'ai ajouté pleins de dossiers mdr

--- routeur.adb
+++
@@ -1,5 +0,0 @@
-with Ada.Text_IO;           use Ada.Text_IO;
-with Ada.Integer_Text_IO;   use Ada.Integer_Text_IO;
-with SDA_Exceptions; 		use SDA_Exceptions;
-with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
-
---
+++ src/routeur.adb
@@ -0,0 +1,6 @@
+procedure Routeur is
+
+begin
+   --  Insert code here.
+   null;
+end Routeur;
