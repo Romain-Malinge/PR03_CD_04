fd9b069713c08222edddc3c9689f639d12da710e
romain.malinge.pro@gmail.com <romain.malinge.pro@gmail.com>
2022-12-10 15:42:06 +0100

    j'ai fait trucs

--- programme/routeur.adb
+++ programme/routeur.adb
@@ -10,85 +10,97 @@ with LCA_IP; use LCA_IP;
 
 procedure Routeur is
 
-     --Définition des types secondaires
-     type E_Politique is (FIFO,LRU,LFU);
+    ------------------ Définition des types secondaires
+
+    UN_OCTET : constant T_Adresse_IP := 2 ** 8;
+    type E_Politique is (FIFO,LRU,LFU);
 
 
-     --Espace pour les sous-programmes
+    ------------------ Espace pour les sous-programmes
 
-     -- Transformer un String en un Unbounded_String
-     function "+" (Item : in String) return Unbounded_String
+    --
+    procedure Set_IP (IP : in out T_Adresse_IP; N1 : Integer; N2 : Integer; N3 : Integer; N4 : Integer) is
+    begin
+        IP := N1;
+        IP := IP * UN_OCTET + N2;
+        IP := IP * UN_OCTET + N3;
+        IP := IP * UN_OCTET + N4;
+    end Set_IP;
+
+
+    -- Transformer un String en un Unbounded_String
+    function "+" (Item : in String) return Unbounded_String
                    renames To_Unbounded_String;
 
-     -- Transformer un Unbounded_String en un String
-     function "-" (Item : in Unbounded_String) return String
+    -- Transformer un Unbounded_String en un String
+    function "-" (Item : in Unbounded_String) return String
                    renames To_String;
 
-     -- Vérifier que le motif .txt est dans un Unbounded_String
-     function txt_present (Mot : in Unbounded_String) return Boolean is
-          Taille : Integer;
-     begin
-          Taille := Length(Mot);
-          if Taille < 4 then
-               return False;
-          else
-               return ( (Mot(Taille) = +"t") and (Mot(Taille-1) = +"x") and (Mot(Taille-2) = +"t") and (Mot(Taille-3) = +".") );
-          end if;
-     end txt_present;
+    -- Vérifier que le motif .txt est dans un Unbounded_String
+    function txt_present (Mot : in Unbounded_String) return Boolean is
+        Taille : Integer;
+    begin
+        Taille := Length(Mot);
+        if Taille < 4 then
+            return False;
+        else
+            return ( (Mot(Taille) = +"t") and (Mot(Taille-1) = +"x") and (Mot(Taille-2) = +"t") and (Mot(Taille-3) = +".") );
+        end if;
+    end txt_present;
 
-     -- Variables globales du programme
+    ------------------ Variables globales du programme
 
-     Paquet : File_Type;
-     Nom_Paquet : Unbounded_String;
-     Nom_Table : Unbounded_String;
-     Nom_Resultat : Unbounded_String;
-     Taille_Cache : Integer;
-     Politique : E_Politique;
-     Fin : Boolean;
-     Bavard : Boolean;
-     Nbr_Ajoute : Interger;
+    Paquet : File_Type;
+    Nom_Paquet : Unbounded_String;
+    Nom_Table : Unbounded_String;
+    Nom_Resultat : Unbounded_String;
+    Taille_Cache : Integer;
+    Politique : E_Politique;
+    Fin : Boolean;
+    Bavard : Boolean;
+    Nbr_Ajoute : Interger;
 
 
 begin
 
-     -- Initialisation des variables
+    -- Initialisation des variables
 
-     Nom_Paquet := "paquet.txt";
-     Nom_Table := "table.txt";
-     Nom_Resultat := "resultats.txt";
-     Taille_Cache := 10;
-     Politique := FIFO;
-     Bavard := True;
-     Fin := Faux;
-     Nbr_Ajoute := 0;
+    Nom_Paquet := "paquet.txt";
+    Nom_Table := "table.txt";
+    Nom_Resultat := "resultats.txt";
+    Taille_Cache := 10;
+    Politique := FIFO;
+    Bavard := True;
+    Fin := Faux;
+    Nbr_Ajoute := 0;
 
-     -- Traiter les arguments de la ligne de commande
-     for a in 1..Argument_Count loop
+    -- Traiter les arguments de la ligne de commande
+    for a in 1..Argument_Count loop
 
-          -- Traiter l'argument a de la ligne de commande
+        -- Traiter l'argument a de la ligne de commande
 
-          case Argument(a) is
-               when +"-c" => Taille_Cache := Argument(a+1);
-               when +"-P" => Politique := Argument(a+1);
-               when +"-S" => Bavard := False;
-               when +"-p" => Nom_Paquet := Argument(a+1);
-               when +"-t" => Nom_Table := Argument(a+1);
-               when +"-r" => Nom_Resultat := Argument(a+1);
-               when others => null;
-          end case;
+        case Argument(a) is
+            when +"-c" => Taille_Cache := Argument(a+1);
+            when +"-P" => Politique := Argument(a+1);
+            when +"-S" => Bavard := False;
+            when +"-p" => Nom_Paquet := Argument(a+1);
+            when +"-t" => Nom_Table := Argument(a+1);
+            when +"-r" => Nom_Resultat := Argument(a+1);
+            when others => null;
+        end case;
 
-          -- Traiter les exceptions
+        -- Traiter les exceptions
 
-          if Taille_Cache < 1 then
-               raise Taille_Cache_Error;
-          elsif txt_present(Nom_Paquet) then
-               raise Not_Txt_Error;
-          elsif txt_present(Nom_Table) then
-               raise Not_Txt_Error;
-          elsif txt_present(Nom_Cache) then
-               raise Not_Txt_Error;
-          end if;
+        if Taille_Cache < 1 then
+            raise Taille_Cache_Error;
+        elsif txt_present(Nom_Paquet) then
+            raise Not_Txt_Error;
+        elsif txt_present(Nom_Table) then
+            raise Not_Txt_Error;
+        elsif txt_present(Nom_Cache) then
+            raise Not_Txt_Error;
+        end if;
 
-     end loop;
+    end loop;
 
 end;
